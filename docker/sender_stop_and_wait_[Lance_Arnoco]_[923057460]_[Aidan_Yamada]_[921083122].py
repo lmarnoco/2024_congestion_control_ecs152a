39b73caf92bbc0e2c3a1ba444774195e7f29bdbb
lmarnoco <lmarnoco@ucdavis.edu>
2026-02-07 15:15:28 -0800

    Prep Work Done

--- docker/sender_stop_and_wait_[Lance_Arnoco]_[923057460]_[Aidan_Yamada]_[921083122].py
+++ docker/sender_stop_and_wait_[Lance_Arnoco]_[923057460]_[Aidan_Yamada]_[921083122].py
@@ -4,6 +4,7 @@ import socket
 PACKET_SIZE = 1024
 SEQ_ID_SIZE = 4
 MESSAGE_SIZE = PACKET_SIZE - SEQ_ID_SIZE
+#Use as Iterator
 EXPECTED_SEQ_ID = 0
 
 #Data that has been sent but has not received ACK Back from Receiver
@@ -20,6 +21,7 @@ Src_PORT = 4500
 
 Dst_PORT = 5001
 
+
 #Convert Mp3 to Byte File
 file_path = 'file.mp3'
 with open(file_path, 'rb') as file:
@@ -31,12 +33,29 @@ with open(file_path, 'rb') as file:
 def create_FINACK(seq_id, message):
     return int.to_bytes(seq_id, SEQ_ID_SIZE, signed=True, byteorder='big') + message.encode()
 
-def parse_message()
+def parse_message():
+    #Parse bits like [Packet Size * Expected ID]
+    message = ""
+    return message
+
 
 
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as udp_socket:
     #Bind Socket to IP and Port
     udp_socket.bind(HOST, Src_PORT)
+
+    #If received Correct Ack and Send FINACK Increment Expected_SEQ_ID (Else will just resend message)
+
+    #Get Message
+    message = parse_message
+
+    #Put Message into Socket
+
+    #Send Socket
+
+    
+
+
 
     
 
