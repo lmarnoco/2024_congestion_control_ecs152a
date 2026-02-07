6ebffb70b82da085261c9715a56211e937edd921
lmarnoco <lmarnoco@ucdavis.edu>
2026-02-07 15:29:50 -0800

    New Parser thing from Aidan

--- docker/sender_stop_and_wait_[Lance_Arnoco]_[923057460]_[Aidan_Yamada]_[921083122].py
+++ docker/sender_stop_and_wait_[Lance_Arnoco]_[923057460]_[Aidan_Yamada]_[921083122].py
@@ -17,31 +17,34 @@ FINACKS_EXPECTED_TO_SEND = {}
 #IP Address and Host
 HOST = '0.0.0.0'
 #Possibly change
-Src_PORT = 4500
+udp_ip = 4500
 
-Dst_PORT = 5001
+dst_port = 5001
 
+socket_client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # IPv4, UDP
 
 #Convert Mp3 to Byte File
 file_path = 'file.mp3'
 with open(file_path, 'rb') as file:
-    mp3_byte = file.read()
+    while True:
+        mp3_byte = file.read(1020)
+
+        if not mp3_byte:
+            break
+        else:
+            seq_bytes = EXPECTED_SEQ_ID.to_bytes(SEQ_ID_SIZE)
+            mp3_byte = mp3_byte.to_bytes(MESSAGE_SIZE)
+            mp3_byte.insert(0, seq_bytes)
+            encoded_message = mp3_byte.encode()
+            socket_client.sendto(encoded_message, (udp_ip, dst_port))
+            print(f"Sent message: {mp3_byte} to {udp_ip}:{dst_port}") 
+
 
 
 #Creates Final+ACK to send back to receiever
 
 def create_FINACK(seq_id, message):
     return int.to_bytes(seq_id, SEQ_ID_SIZE, signed=True, byteorder='big') + message.encode()
-
-
-def parse_message():
-    #Parse bits like [Packet Size * Expected ID]
-    message = int.to_bytes(EXPECTED_SEQ_ID) + mp3_byte[EXPECTED_SEQ_ID * 1020].encode('utf-8')
-    
-    #Converts message to a string that can later be returned as integers
-    return message
-
-
 
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as udp_socket:
     #Bind Socket to IP and Port
