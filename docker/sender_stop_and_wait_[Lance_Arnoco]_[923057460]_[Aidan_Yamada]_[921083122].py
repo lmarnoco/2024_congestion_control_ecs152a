9b1b50a0bf3737c315f8d39e7185c02439b4c600
lmarnoco <lmarnoco@ucdavis.edu>
2026-02-07 13:58:14 -0800

    Test

---
+++ docker/sender_stop_and_wait_[Lance_Arnoco]_[923057460]_[Aidan_Yamada]_[921083122].py
@@ -0,0 +1,19 @@
+import socket
+
+PACKET_SIZE = 1024
+SEQ_ID_SIZE = 4
+MESSAGE_SIZE = PACKET_SIZE - SEQ_ID_SIZE
+EXPECTED_SEQ_ID = 0
+
+#Data that has been sent but has not received ACK Back from Receiver
+SENT_DATA = {}
+#SEQ ID Acks that were returned
+ACK_RECEVID = {}
+#ACKS Sent Back to Rec
+FINACKS_SENT = {}
+
+#Creates Final+ACK to send back to receiever
+
+def create_FINACK(seq_id, message):
+    return int.to_bytes(seq_id, SEQ_ID_SIZE, signed=True, byteorder='big') + message.encode()
+
