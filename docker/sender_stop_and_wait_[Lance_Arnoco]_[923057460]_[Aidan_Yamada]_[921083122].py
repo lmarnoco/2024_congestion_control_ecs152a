8e5b88e2d285844be66bbfb205b270ed98f41b5f
Lance Arnoco <lmarnoco@ucdavis.edu>
2026-02-11 23:46:20 -0800

    Idk How to test it

--- docker/sender_stop_and_wait_[Lance_Arnoco]_[923057460]_[Aidan_Yamada]_[921083122].py
+++ docker/sender_stop_and_wait_[Lance_Arnoco]_[923057460]_[Aidan_Yamada]_[921083122].py
@@ -17,28 +17,28 @@ FINACKS_EXPECTED_TO_SEND = {}
 #IP Address and Host
 HOST = '0.0.0.0'
 #Possibly change
-udp_ip = 4500
+src_port = 4500
 
 dst_port = 5001
 
 socket_client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # IPv4, UDP
 
 #Convert Mp3 to Byte File
-file_path = 'file.mp3'
-with open(file_path, 'rb') as file:
-    while True:
-        mp3_byte = file.read(1020)
 
-        if not mp3_byte:
-            break
-        else:
+def parse_message(): 
+    file_path = 'file.mp3'
+    with open(file_path, 'rb') as file:
+            file.seek(1020 * EXPECTED_SEQ_ID)
+            # 
+            # mp3_byte = mp3_byte.to_bytes(MESSAGE_SIZE)
+            # mp3_byte.insert(0, seq_bytes)
+            encoded_message = file.read(MESSAGE_SIZE)
+            if not encoded_message:
+                 return None
+            
             seq_bytes = EXPECTED_SEQ_ID.to_bytes(SEQ_ID_SIZE)
-            mp3_byte = mp3_byte.to_bytes(MESSAGE_SIZE)
-            mp3_byte.insert(0, seq_bytes)
-            encoded_message = mp3_byte.encode()
-            socket_client.sendto(encoded_message, (udp_ip, dst_port))
-            print(f"Sent message: {mp3_byte} to {udp_ip}:{dst_port}") 
-
+            message_bytes = seq_bytes + encoded_message
+            return message_bytes
 
 
 #Creates Final+ACK to send back to receiever
@@ -46,18 +46,35 @@ with open(file_path, 'rb') as file:
 def create_FINACK(seq_id, message):
     return int.to_bytes(seq_id, SEQ_ID_SIZE, signed=True, byteorder='big') + message.encode()
 
+
+
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as udp_socket:
     #Bind Socket to IP and Port
-    udp_socket.bind(HOST, Src_PORT)
+    udp_socket.bind((HOST, src_port))
+    udp_socket.settimeout(1)
 
     #If received Correct Ack and Send FINACK Increment Expected_SEQ_ID (Else will just resend message)
 
-    #Get Message
-    message = parse_message
+    while True:
+        try:
+            data, address = udp_socket.recvfrom(PACKET_SIZE)
 
-    #Put Message into Socket
+            ack_id = data[:SEQ_ID_SIZE]
 
-    #Send Socket
+            seq_ack = int.from_bytes(ack_id, byteorder='big')
+
+            if seq_ack == EXPECTED_SEQ_ID:
+                 udp_socket.sendto(create_FINACK(EXPECTED_SEQ_ID, "==FINACK=='"), (HOST, dst_port))
+                 EXPECTED_SEQ_ID += 1
+
+
+        except socket.timeout:
+            #Get Message
+            message = parse_message()
+            #Put Message into Socket and Send
+            udp_socket.sendto(message, (HOST, dst_port))
+            print(f"Sent message: {EXPECTED_SEQ_ID} to {src_port}:{dst_port}") 
+
 
 
 
